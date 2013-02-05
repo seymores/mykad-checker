e05c8c352295317e7f9c34fa14fd7d228a1eab53
ping <ping@favoritemedium.com>
2013-02-05 22:28:30 +0800

    Clean up

--- src/mykad_checker/core.clj
+++ src/mykad_checker/core.clj
@@ -1,22 +1,22 @@
 (ns mykad-checker.core
   (:use [net.cgrand.enlive-html :as html])
-  (:require [clj-http.client :as client]))
-
-(import 'java.io.ByteArrayInputStream)
+  (:require [clj-http.client :as client])
+  (:import [java.io ByteArrayInputStream]))
 
 (def site-url "http://daftarj.spr.gov.my/NEWDAFTARJ/DaftarjBM.aspx")
-(def headers {"Content-type" "application/x-www-form-urlencoded"
-              "Accept" "text/plain"})
 
-(defn string-to-stream 
+(defn- string-to-stream 
   "Convert string to inputstream"
   [string]
   (ByteArrayInputStream. (.getBytes (.trim string))))
 
-(defn fetch-url 
+(defn- fetch-url 
   [url]
   "Grab the page"
   (html/html-resource (java.net.URL. url)))
+
+(defn- only-val [elem]
+  (.trim (first (:content (first elem)))))
 
 (defn get-params 
   "Extract necessary params variables from page"
@@ -27,25 +27,16 @@
     {"__VIEWSTATE" viewstate "__EVENTVALIDATION" event-vali "__EVENTTARGET" "" 
      "__EVENTARGUMENT" "" "Semak" "Semak" "txtIC" ic-number}))
 
-
 (defn submit-check
   "Form submit params to check start check"
   [params]
   (let [output (client/post site-url {:form-params params})
         html (html/html-resource (string-to-stream (:body output)))] html))
 
-(defn- prepare-params
-  "Simplify the preparetion of the vars and params needed"
-  [icno]
-  (get-params (fetch-url site-url) icno))
-
-(defn- only-val [elem]
-  (.trim (first (:content (first elem)))))
-
 (defn extract-result 
   "Extract the result from the form submit"
   [page]
-  (when-not (empty? (html/select page [:td.kerolclass]))
+  (when (seq (html/select page [:td.kerolclass]))
     {:nama    (only-val (html/select page [:span#Labelnama]))
      :ic-no   (only-val (html/select page [:span#LabelIC]))
      :dob     (only-val (html/select page [:span#LabelTlahir]))
@@ -56,6 +47,13 @@
      :negeri  (only-val (html/select page [:span#Labelnegeri]))
      :parli   (only-val (html/select page [:span#Labelpar]))
      :status  (only-val (html/select page [:span#LABELSTATUSDPI]))}))
+
+(defn prepare-params
+  "Simplify the preparetion of the vars and params needed"
+  [icno]
+  (get-params (fetch-url site-url) icno))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn check-mykad
   "Check the ic with the given ic number"
