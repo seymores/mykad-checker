a3efa9bba069752fcd628626d320bb56c6c22be0
ping <ping@favoritemedium.com>
2013-02-05 18:20:35 +0800

    Feature complete

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject mykad-checker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "MyKAD checker in clj -- clone of https://github.com/Sinar/mykadspr/blob/master/py/mykadspr.py"
+  :url "https://github.com/seymores/mykad-checker"
+  :license {:name "GPL v3" :url "http://www.gnu.org/licenses"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [enlive "1.0.0"]
+                 [clj-http "0.6.4"]
                  [com.taoensso/carmine "1.4.0"]
                  [org.clojure/data.csv "0.1.2"]])
+
+; Usage: (check-mykad "790222075569")
--- src/mykad_checker/core.clj
+++ src/mykad_checker/core.clj
@@ -1,17 +1,63 @@
 (ns mykad-checker.core
-  (:use [net.cgrand.enlive-html :as html]))
+  (:use [net.cgrand.enlive-html :as html])
+  (:require [clj-http.client :as client]))
 
-(def SITE-URL "http://daftarj.spr.gov.my/NEWDAFTARJ/DaftarjBM.aspx")
+(import 'java.io.ByteArrayInputStream)
 
-(defn fetch-url [url]
+(def site-url "http://daftarj.spr.gov.my/NEWDAFTARJ/DaftarjBM.aspx")
+(def headers {"Content-type" "application/x-www-form-urlencoded"
+              "Accept" "text/plain"})
+
+(defn string-to-stream 
+  "Convert string to inputstream"
+  [string]
+  (ByteArrayInputStream. (.getBytes (.trim string))))
+
+(defn fetch-url 
+  [url]
   "Grab the page"
- (html/html-resource (java.net.URL. url)))
+  (html/html-resource (java.net.URL. url)))
 
 (defn get-params 
   "Extract necessary params variables from page"
   [site ic-number]
-  (let [inputs (html/select site #{[:input#__VIEWSTATE] [:input#__EVENTVALIDATION]})
-        viewstate (first inputs)
-        event-vali (second inputs)]
-    {"__VIEWSTATE" viewstate "__EVENTVALIDATION" event-vali "__EVENTTARGET" "" "__EVENTARGUMENT" "" "Semak" "CHECK" "txtIC" ic-number}))
+  (let [inputs      (html/select site #{[:input#__VIEWSTATE] [:input#__EVENTVALIDATION]})
+        viewstate   (:value (:attrs (first inputs)))
+        event-vali  (:value (:attrs (second inputs)))]
+    {"__VIEWSTATE" viewstate "__EVENTVALIDATION" event-vali "__EVENTTARGET" "" 
+     "__EVENTARGUMENT" "" "Semak" "Semak" "txtIC" ic-number}))
 
+(defn prepare-params
+  [icno]
+  (get-params (fetch-url site-url) icno))
+
+(defn submit-check
+  "Form submit params to check start check"
+  [params]
+  (let [output (client/post site-url {:form-params params})
+        html (html/html-resource (string-to-stream (:body output)))] html))
+
+(defn only-val [elem]
+  (.trim (first (:content (first elem)))))
+
+(defn extract-result 
+  "Extract the result from the form submit"
+  [page]
+  (when-not (empty? (html/select page [:td.kerolclass]))
+    {:nama    (only-val (html/select page [:span#Labelnama]))
+     :ic-no   (only-val (html/select page [:span#LabelIC]))
+     :dob     (only-val (html/select page [:span#LabelTlahir]))
+     :jantina (only-val (html/select page [:span#Labeljantina]))
+     :loc     (only-val (html/select page [:span#Labellokaliti]))
+     :daerah  (only-val (html/select page [:span#Labeldm]))
+     :dun     (only-val (html/select page [:span#Labeldun]))
+     :negeri  (only-val (html/select page [:span#Labelnegeri]))
+     :parli   (only-val (html/select page [:span#Labelpar]))
+     :status  (only-val (html/select page [:span#LABELSTATUSDPI]))}))
+
+(defn check-mykad
+  "Check the ic with the given ic number"
+  [icno]
+  (-> (prepare-params icno)
+      (submit-check)
+      (extract-result)))
